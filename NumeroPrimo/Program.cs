1efcfcd51c6a193ffb338c7fbf573b00fbe1cd88
masfazan <msfazan@icloud.com>
2024-04-22 09:51:56 -0300

    Add project files.

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,26 @@
+﻿int num = 0, contador = 1;
+Console.WriteLine("Digite um número: ");
+num = int.Parse(Console.ReadLine());
+if (num > 1)
+{
+    for (int i = num; i > 1; i--)
+    {
+        if (num % i == 0)
+        {
+            contador++;
+        }
+    }
+    if (contador == 2)
+    {
+        Console.WriteLine("O número é primo!");
+    }
+    else
+    {
+        Console.WriteLine("O número não é primo!");
+    }
+}
+else
+{
+    Console.WriteLine("O número deve ser maior que 1");
+}
+            
